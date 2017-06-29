631150c8d237e26faa8097f98ffa303f63924e06
pcsailor <Phil@PhilCurtis.io>
2017-06-29 14:22:01 -0500

    video files/folders added

--- video-player/video-player.js
+++ video-player/video-player.js
@@ -1,4 +1,8 @@
+
+
+
 // still need to add a full screen button but this is a detailed HTML5 video player
+
 // Get elements on page
 const player = document.querySelector('.player'); // get the player
 const video = player.querySelector('.viewer'); // gets actual video
@@ -7,6 +11,9 @@ const progressBar = player.querySelector('.progress__filled'); // gets progress_
 const toggle = player.querySelector('.toggle'); // gets toggle
 const skipButtons = player.querySelectorAll('[data-skip]'); // gets skip buttons
 const ranges = player.querySelectorAll('.player__slider'); // gets player slider
+console.log("player = ", player);
+console.log("video = ", video);
+
 // Build out functions
 // 01_Toggle to either play or pause video
 function togglePlay() { // entering 'togglePlay()' in console plays/pauses video
@@ -23,12 +30,14 @@ function togglePlay() { // entering 'togglePlay()' in console plays/pauses video
     // version03 - use a ternery operator without variable
     video[video.paused ? 'play' : 'pause']();
 }
+
 // Note: to change play/pause icons, best way is listen to video for pause.  Whatever causes video pause (ie: plugin) will change button icon.  // This doesn't tie into the 'function togglePlay' & only listens to video condition
 function updateButton() {
     console.log('updateButton clicked');
     const icon = this.paused ? '►' : '❚ ❚'; // notice paused, not pause
     toggle.textContent = icon;
 }
+
 // Skip functionss
 // how much video will be skipped - goes to DOM buttons
 function skip() {
@@ -37,11 +46,16 @@ function skip() {
     console.log('this.dataset.skip = ', this.dataset.skip);
     video.currentTime += parseFloat(this.dataset.skip); // 'this.dataset.skip' is a string so 'parseFloat converts to a number.
 }
+
+
+
 function handleRangeUpdate() { // slider adjustments for volume & playback rate speed
     console.log(this.name);
     console.log(this.value);
     video[this.name] = this.value;
 }
+
+
 // Progress bar (the big one!)
 // should be updating in real time as video plays, and
 // clicking on bar & dragging should update video playback location
@@ -51,6 +65,7 @@ function handleProgress() {
     const percent = (video.currentTime / video.duration) * 100; // currentTime duration are properties on the video // this can be seen by going to the video source --> Inspect --> Elements --> Properties --> Video (dropdown) // * 100 is for whole number percentages
     progressBar.style.flexBasis = `${percent}%`; // 'progressBar' from top variable
 }
+
 // Scrub (the big one!)
 // whereever clicked on bar, scrub the video to that relative point
 function scrub(e) {
@@ -58,18 +73,25 @@ function scrub(e) {
     const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration; // divided by width of entire progress bar // multply by video duration
     video.currentTime = scrubTime; // simply update the video
 }
+
+
 // Hook up event listeners
 // once have togglePlay function, need to hook it up to the video and the button
 video.addEventListener('click', togglePlay); // plays/pauses video when video window clicked
 toggle.addEventListener('click', togglePlay); // plays/pauses video when play button clicked
+
+
 // video.play() or video.pause() to play/pause video from console
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+
 // listen for video to emit an event called 'timeupdate' & when that happens code will call function handleProgress()
 video.addEventListener('timeupdate', handleProgress); // event called 'progress' and both will trigger when video is updating it's time code so if paused will not unnecessirily run the function
+
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
 //Progress Bar Scrubbing
 progress.addEventListener('click', scrub); // when click on progress bar, run scrub function
 // progress.addEventListener('mousemove', scrub); // this reacts when mouse moves over progress bar resulting in jerky video.
@@ -80,4 +102,5 @@ let mousedown = false;
 // v02
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e)); // When mouse moves, mousedown and then scrub.  1st checks mousedown variable, if true-->then run scrub function.  If mousedown is false, then returns false doing nothing.
 progress.addEventListener('mousedown', () => mousedown = true);
-progress.addEventListener('mouseup', () => mousedown = false);``
+progress.addEventListener('mouseup', () => mousedown = false);
+console.log("video-player.js is run");
