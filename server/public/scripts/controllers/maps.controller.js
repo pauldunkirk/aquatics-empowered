48d7a7b8bebc67b46da11b6d03522f758c5472a1
Jeremy Leif <jeremy.leif@gmail.com>
2017-07-06 13:34:00 -0500

    added pools to map

--- server/public/scripts/controllers/maps.controller.js
+++ server/public/scripts/controllers/maps.controller.js
@@ -1,22 +1,22 @@
 app.controller('MapsController', ['$location', function($location) {
 var self = this;
 
-
-self.myMap = function (){
-      myCenter = new google.maps.LatLng(44.9778, -93.2650);
-      var mapOptions = {
-          center: myCenter,
-          zoom: 12,
-          scrollwheel: false,
-          draggable: true,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-      var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
-
-      var marker = new google.maps.Marker({
-          position: myCenter,
-      });
-      marker.setMap(map);
-  };
+// 
+// self.myMap = function (){
+//       myCenter = new google.maps.LatLng(44.9778, -93.2650);
+//       var mapOptions = {
+//           center: myCenter,
+//           zoom: 12,
+//           scrollwheel: false,
+//           draggable: true,
+//           mapTypeId: google.maps.MapTypeId.ROADMAP
+//       };
+//       var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+//
+//       var marker = new google.maps.Marker({
+//           position: myCenter,
+//       });
+//       marker.setMap(map);
+//   };
 
 }]);
