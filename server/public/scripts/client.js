59bbb176a8125f9634bc3891c1395a758c59269d
Paul Dunkirk <pauldunkirk@gmail.com>
2017-07-10 19:20:31 -0500

    new water styling

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,174 +1,29 @@
-var app = angular.module('myApp', ['ui.router','ui.bootstrap']);
-// var myApp = angular.module('myApp', ['ui.router','ui.bootstrap','ngAnimate', 'ngTouch', 'ngFader']);
+var app = angular.module('myApp', ['ngRoute','ui.bootstrap']);
+// ui.bootstrap allows toggleable nav bar to begin collapsed- see nav.html
+//possibly in the future, use these tools:
+// var myApp = angular.module('myApp', ['ngRoute', 'xeditable', 'firebase', 'ui.bootstrap', 'smart-table', 'angular-touch', 'angular-animate']); //then source in html
 
 
-// eventually separate routing (config) from app module (client) like in IIMN
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+         .when ('/login', {
+          templateUrl: '/views/login.html',
+          controller: 'LoginController',
+          controllerAs: 'login'
+        }).when ('/maps', {
+            templateUrl: '/views/templates/maps.html',
+            controller: 'MapsController',
+            controllerAs: 'maps'
+        }).when ('/home1', {
+            templateUrl: '/views/home1.html',
+            controller: 'Home1Controller',
+            controllerAs: 'home1'
+        }).when ('/home2', {
+            templateUrl: '/views/home2.html',
+            controller: 'Home2Controller',
+            controllerAs: 'home2'
+        }).otherwise ({
+            redirectTo: '/home1'
+        });
 
-// IIMN client:  var app = angular.module('fhatApp', ['ngMaterial','ui.router','firebase','chart.js', 'cfp.hotkeys'])
-// IIMN config:  app.config(function($stateProvider,$urlRouterProvider) {
-//   var main = {
-//   //   name: 'main',
-//   //   url: '/main',
-//   //   controller: 'MainStateController',
-//   //   controllerAs: 'msc',
-//   //   templateUrl: '/views/mainstate.html'
-//   //
-// };
-// $stateProvider.state(profilequestions);
-// //   ETC..
-//
-// //   $urlRouterProvider.when('', '/login');
-// });
-
-
-
-
-//   IIMN APP
-// app.config(function($stateProvider,$urlRouterProvider) {
-//   var main = {
-//     name: 'main',
-//     url: '/main',
-//     controller: 'MainStateController',
-//     controllerAs: 'msc',
-//     templateUrl: '/views/mainstate.html'
-//   };
-//   var login = {
-//     name: 'login',
-//     url: '/login',
-//     controller: 'LoginController',
-//     controllerAs: 'lc',
-//     templateUrl:'/views/login.html'
-//   };
-//   var mainState = {
-//     name: 'main',
-//     url: '/main',
-//     controlller: 'MainController',
-//     controllerAs: 'mc',
-//     templateUrl: '/views/mainstate.html'
-//   };
-//   ETC..
-//   BELOW NO URL SPECIFIED - CHILDREN OF MAIN - MAIN.something
-//   var budget = {
-//     name: 'main.budget',
-//     controller: 'BudgetController',
-//     controllerAs: 'bc',
-//     templateUrl:'/views/budget.html'
-//   };
-//   var budgetIncome = {
-//     name: 'main.budget.income',
-//     controller: 'BudgetController',
-//     controllerAs: 'bc',
-//     templateUrl:'/views/budgetincome.html'
-//   };
-//   ETC..
-//
-//   $stateProvider.state(login);
-//   $stateProvider.state(main);
-//   $stateProvider.state(welcome);
-//   $stateProvider.state(balanceSheet);
-//   $stateProvider.state(balanceSheetAssets);
-//   $stateProvider.state(profilequestions);
-//   ETC..
-
-//   $urlRouterProvider.when('', '/login');
-// }); END IIMN APP
-
-
-
-
-// BIGTOE APP
-// myApp.config(['$routeProvider', function($routeProvider) {
-//     $routeProvider
-//         .when ('/login', {
-//           templateUrl: '/views/login.html',
-//           controller: 'LoginController',
-//           controllerAs: 'login'
-//         }).when ('/requests', {
-//             templateUrl: '/views/requests.html',
-//             controller: 'LoginController',
-//             controllerAs: 'requests'
-//         }).when ('/gigs', {
-//             templateUrl: '/views/gigs.html',
-//             controller: 'GigsController',
-//             controllerAs: 'gigs'
-//         }).when ('/players', {
-//             templateUrl: '/views/players.html',
-//             controller: 'GigsController',
-//             controllerAs: 'players'
-//         }).when ('/mp3s', {
-//             templateUrl: '/views/mp3s.html',
-//             controller: 'mp3sController',
-//             controllerAs: 'mp3s'
-//         }).when ('/charts', {
-//             templateUrl: '/views/charts.html',
-//             controller: 'ChartsController',
-//             controllerAs: 'charts'
-//         }).when ('/contacts', {
-//             templateUrl: '/views/contacts.html',
-//             controller: 'ContactsController',
-//             controllerAs: 'contacts'
-//         }).when ('/setlists', {
-//             templateUrl: '/views/setlists.html',
-//             controller: 'SetlistsController',
-//             controllerAs: 'setlists'
-//         }).when ('/video', {
-//             templateUrl: '/views/video.html',
-//             controller: 'VideoController',
-//             controllerAs: 'video'
-//         }).otherwise ({
-//             redirectTo: '/login'
-//         });
-//
-// }]); //end routeProvider confg BIG token
-
-
-
-
-// OLD AQUATICS APP
-// routing
-// angular.module("aquaticsApp")
-//   .config(function($routeProvider, $locationProvider) {
-//     $locationProvider.html5Mode(true);
-//
-//     $routeProvider
-//       .when("/newUser", {
-//         templateUrl: "views/register.html",
-//         controller: "RegisterController as register"
-//       })
-//       .when("/admin", {
-//         templateUrl: "views/admin.html",
-//         controller: "AdminController as admin"
-//       })
-//       .when("/login", {
-//         templateUrl: "views/login.html",
-//         controller: "LoginController as login"
-//     })
-//       .when("/facilitydetails", {
-//         templateUrl: "views/facilitydetails.html",
-//         controller: "FacilityDetailsController as facilitydetails"
-//       })
-//       .when("/profile", {
-//         templateUrl: "views/profileView.html",
-//         controller: "userProfileController as user"
-//       })
-//       .otherwise( {
-//         templateUrl: "views/home.html",
-//         controller: "HomeController as home",
-//         //authRequired: true
-//       });
-//   });
-  // .run(function($rootScope, $location, $route, AuthService) {
-  //   $rootScope.$on("$routeChangeStart", function(event, next, current) {
-  //     AuthService.checkLoginStatus().then(function(loggedIn) {
-  //       console.log(loggedIn);
-  //       if (next.authRequired && !loggedIn) {
-  //         $location.path("/login");
-  //         $route.reload();
-  //       }
-  //     });
-  //   });
-  // });  END AQUATICS APP
-
-
-
-//
+}]); //end routeProvider confg
---
+++ server/public/scripts/controllers/home1.controller.js
@@ -0,0 +1,8 @@
+app.controller('Home1Controller', ['$location', function($location) {
+  // myApp.controller('LoginController', ['$firebaseAuth', 'GigsFactory', '$location', function($firebaseAuth, GigsFactory, $location) {
+
+      var self = this;
+
+
+
+  }]);
