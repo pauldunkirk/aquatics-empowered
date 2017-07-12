180ee0b36282f2a75138cbd91728c35f8cfcdb53
Paul Dunkirk <pauldunkirk@gmail.com>
2017-07-12 12:15:25 -0500

    what can I do here?

--- server/public/scripts/controllers/nav.controller.js
+++ server/public/scripts/controllers/nav.controller.js
@@ -3,9 +3,9 @@ app.controller('NavController', ['$location', function($location) {
 
       var self = this;
 
-
-      self.isCollapsed = true;
-      self.toggleNav = function(){
-      self.isCollapsed = !self.isCollapsed;
-    };
+    // 
+    //   self.isCollapsed = true;
+    //   self.toggleNav = function(){
+    //   self.isCollapsed = !self.isCollapsed;
+    // };
   }]);
