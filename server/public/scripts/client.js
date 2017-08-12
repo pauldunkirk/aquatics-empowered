a3c64f6d8bb38f2c7b27673291a59aebc45b2598
Jake Iwen <iwenx006@umn.edu>
2017-08-11 20:56:13 -0500

    implemented better angular map directive

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', ['ngRoute','ui.bootstrap', 'angular-google-map']);
+var app = angular.module('myApp', ['ngRoute','ui.bootstrap', 'uiGmapgoogle-maps']);
 // ui.bootstrap allows toggleable nav bar to begin collapsed- see nav.html
 //possibly in the future, use these tools:
 // var myApp = angular.module('myApp', ['ngRoute', 'xeditable', 'firebase', 'ui.bootstrap', 'smart-table', 'angular-touch', 'angular-animate']); //then source in html
