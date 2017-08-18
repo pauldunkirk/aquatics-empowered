571bc10a0764523c4ba5e99eea472729f0ff6557
Jake Iwen <iwenx006@umn.edu>
2017-08-18 02:11:14 -0500

    comments and TODOs

--- server/public/scripts/controllers/maps.controller.js
+++ server/public/scripts/controllers/maps.controller.js
@@ -1,18 +1,19 @@
 app.controller('MapsController', ['$http', 'NgMap', 'GeoCoder', function($http, NgMap, GeoCoder) {
   const vm = this;
-  const defaultCenter = [44.9778, -93.2650];
+  const defaultCenter = [44.9778, -93.2650]; //Minneapolis coords
 
   vm.markerList = [];
-  vm.mapCenter = defaultCenter; //Minneapolis coords
+  vm.mapCenter = defaultCenter;
 
   NgMap.getMap().then( map => {
     vm.map = map; //to access gMaps API features
     vm.distance = google.maps.geometry.spherical.computeDistanceBetween;
     console.log('map', map);
     console.log('markers', map.markers);
-    // vm.mapCenter = new google.maps.LatLng(vm.mapCenter[0], vm.mapCenter[1])
-    getPools(); //run $http request to server
+    // TODO: set map center to location of user (determined with from browser query)
+    getPools(); //run $http request to server for nearby pools
   });
+
   vm.clicked = url => window.open(url); //open website in new tab
 
   vm.showDetail = (e, pool) => {
@@ -31,7 +32,8 @@ app.controller('MapsController', ['$http', 'NgMap', 'GeoCoder', function($http, 
   const getPools = () => {
     $http.get('/poolList/')
     .then( res => {
-      vm.markerList = createMarkerList(res.data);
+      vm.allPools = res.data;
+      vm.markerList = createMarkerList(vm.allPools);
       vm.pool = vm.markerList[0]; //initialize for infoWindow
     },
       res => console.log('GET pools - error:', res)
@@ -39,6 +41,7 @@ app.controller('MapsController', ['$http', 'NgMap', 'GeoCoder', function($http, 
   };
 
   const createMarkerList = poolArray => (
+    //TODO: only do this for the 40 or so markers nearest to vm.map.center
     poolArray.map( pool => (
       { id: pool.id,
         position: [pool.lat, pool.lng],
@@ -46,6 +49,7 @@ app.controller('MapsController', ['$http', 'NgMap', 'GeoCoder', function($http, 
         website: pool.url,
         street_address: pool.street_address,
         city: pool.city,
+        proximityRank: null,
         state: pool.state,
         zip: pool.zip,
         visible: true }
@@ -59,20 +63,21 @@ app.controller('MapsController', ['$http', 'NgMap', 'GeoCoder', function($http, 
       .then( (results, status) => {
         let coords = results[0].geometry.location;
         vm.mapCenter = [coords.lat(), coords.lng()];
+        //TODO: run createMarkerList(vm.allPools) for new proximal marker set
         setMarkerVis(vm.radius);
       });
     } else {
-      //default to large distance for blank field
       setMarkerVis(vm.radius);
     }
   };
 
   const setMarkerVis = radius => {
     for (key in vm.map.markers) {
+      console.log('marker', vm.map.markers[key]);
       let dist =
         vm.distance(vm.map.markers[key].position, vm.map.center) / 1609.3445;
       let inRangeBool = dist < radius;
-      vm.map.markers[key].setVisible(inRangeBool || !radius); //make visible if no set radius
+      vm.map.markers[key].setVisible(inRangeBool || !radius);
     }
   };
 
