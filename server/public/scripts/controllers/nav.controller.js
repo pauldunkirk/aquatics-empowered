c5a69e36d46e9e9d8491c035d0eab42d624ae66d
Paul Dunkirk <pauldunkirk@gmail.com>
2017-09-12 13:47:43 -0500

    toggle nav and map page styling

--- server/public/scripts/controllers/nav.controller.js
+++ server/public/scripts/controllers/nav.controller.js
@@ -2,10 +2,8 @@ app.controller('NavController', ['$location', function($location) {
   // myApp.controller('LoginController', ['$firebaseAuth', 'GigsFactory', '$location', function($firebaseAuth, GigsFactory, $location) {
 
       var self = this;
-
-    // 
-    //   self.isCollapsed = true;
-    //   self.toggleNav = function(){
-    //   self.isCollapsed = !self.isCollapsed;
-    // };
+      self.isCollapsed = true;
+      self.toggleNav = function(){
+      self.isCollapsed = !self.isCollapsed;
+    };
   }]);
