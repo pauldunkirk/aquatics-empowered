7a5e856549f45f7a8f2ea434a47631ae1310081f
Jake Iwen <iwenx006@umn.edu>
2017-09-20 20:21:14 -0500

    refactor

--- server/public/scripts/controllers/admin.controller.js
+++ server/public/scripts/controllers/admin.controller.js
@@ -6,68 +6,75 @@ app.controller('AdminController', ['$http', function($http) {
   const textBase = api + 'place/textsearch/json?query=';
   const radarBase = api + 'place/radarsearch/json?location=';
   const apiKeyEnd = '&key=AIzaSyCAlpI__XCJRk774DrR8FMBBaFpEJdkH1o';
-  const gPlaces = new google.maps.places.PlacesService(document.createElement('div'));
+  const gPlacesAPI = new google.maps.places.PlacesService(document.createElement('div'));
 
   //a JSON containing the 1000 biggest US cities and their coordinates
   vm.cityCoordsUrl = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
   vm.citiesLeft = [0]; //array to allow passing by reference to pulse()
+  vm.placesLeft = [0];
   vm.geocodesLeft = 0;
   vm.errorCount = 0;
   vm.gPlaceIdList = [];
   getFacilities();
 
-  vm.findPlaceIds = (num=1) => {
-    $http.get(vm.cityCoordsUrl).then(
-      res => pulse(searchCity, res.data, vm.citiesLeft, 1000-num),
-      err => console.log('could not find cities JSON', err)
-    )
-  }
-  vm.getPlaceIdList = () => {
-    console.log('wgfveragvaertvbwerhrye');
-    $http.get('/placeIds').then(
-      res => vm.gPlaceIdList = res.data.map( obj => obj.place_id ),
-      err => console.log('error accessing place id table', err)
-    )
-  }
-
-  vm.getGoogleInfo = placeId => {
-    gPlaces.getDetails( {placeId}, (place, status) => {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        console.log('place', place);
-
-
-        let adrCmps = {}; //format address components of gmaps for usability
-        $.each(place.address_components, (k,v1) => $.each(v1.types, (k2, v2) =>
-          adrCmps[v2]=v1.short_name)
-        );
-
-        console.log('adrCmps:', adrCmps);
-        const loc = place.geometry.location;
-        const facility = {
-          name: place.name,
-          street_address: adrCmps.street_number + ' ' + adrCmps.route,
-          city: adrCmps.locality,
-          state: adrCmps.administrative_area_level_1,
-          zip: adrCmps.postal_code,
-          phone: place.formatted_phone_number,
-          image_url: place.icon,
-          url: place.website,
-          coords: [loc.lat(), loc.lng()],
-          google_place_id: place.place_id,
-          google_places_data: JSON.stringify(place, undefined, 4)
+  //methods making heavy use of google places
+  vm.gPlaces = {
+    findIds(num=1) {
+      $http.get(vm.cityCoordsUrl).then(
+        res => pulse(searchCity, res.data, vm.citiesLeft, 180, 1000-num),
+        err => console.log('could not find cities JSON', err)
+      )
+    },
+    getIdList() {
+      $http.get('/placeIds').then(
+        res => vm.gPlaceIdList = res.data.map( obj => obj.place_id ),
+        err => console.log('error accessing place id table', err)
+      )
+    },
+    //takes a list of google Ids, gets relevant info, and adds to db
+    getInfoFromIds(idList) {
+      return pulse(vm.gPlaces.getDetails, idList, vm.placesLeft, 1100)
+    },
+    getDetails(placeId) {
+      gPlacesAPI.getDetails( {placeId}, (place, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          console.log('place', place);
+          const facility = vm.gPlaces.parseDetails(place);
+          addPlaceIdToDb(facility);
         }
-        addFacility(facility);
+      });
+    },
+    //put google place details into format for our DB
+    parseDetails(pResult) {
+      let adrCmps = {}; //format address components of gmaps for usability
+      $.each(pResult.address_components, (k,v1) => $.each(v1.types, (k2, v2) =>
+        adrCmps[v2]=v1.short_name)
+      );
+      const loc = pResult.geometry.location;
+      return {
+        name: pResult.name,
+        street_address: adrCmps.street_number + ' ' + adrCmps.route,
+        city: adrCmps.locality,
+        state: adrCmps.administrative_area_level_1,
+        zip: adrCmps.postal_code,
+        phone: pResult.formatted_phone_number,
+        image_url: pResult.icon,
+        url: pResult.website,
+        coords: [loc.lat(), loc.lng()],
+        google_place_id: pResult.place_id,
+        //ALL of the google details are kept in this JSON
+        google_places_data: JSON.stringify(pResult, undefined, 4)
       }
-    });
+    }
   }
 
-  function pulse(queryFn, list, remaining, index=0) {
+  function pulse(queryFn, list, remaining, delay, index=0) {
     remaining[0] = list.length - index;
     if (index < list.length) {
       setTimeout( () => {
         queryFn(list[index++]);
-        pulse(queryFn, list, remaining, index);
-      }, 180);
+        pulse(queryFn, list, remaining, delay, index);
+      }, delay);
     }
   }
 
@@ -80,7 +87,7 @@ app.controller('AdminController', ['$http', function($http) {
       location: location
     }
 
-    gPlaces.radarSearch(request, (results, status) => {
+    gPlacesAPI.radarSearch(request, (results, status) => {
       if (status !== google.maps.places.PlacesServiceStatus.OK) {
         console.error(status);
         return;
@@ -89,7 +96,7 @@ app.controller('AdminController', ['$http', function($http) {
         { coords: [pool.geometry.location.lat(), pool.geometry.location.lng()],
           google_place_id: pool.place_id }
       ) )
-      for (const facility of facilities) addFacility(facility);
+      for (const facility of facilities) addFacilityToDb(facility);
       }
     );
   };
@@ -101,88 +108,53 @@ app.controller('AdminController', ['$http', function($http) {
     );
   };
 
-  vm.convert = listType => {
-    switch (listType) {
-      case "google":
-          googleFn(vm.text);
-        break;
-      case "mapmuse":
-          console.log('mapMuse');
-          mapMuse(vm.text);
-        break;
-      default:
-    }
-  }
-
-  //choose 'paste values only' when pasting to spreadsheet
-  const googleFn = text => {
-    let arr = text.split('\n'); //create array element for each line of text
-    arr = arr.filter( entry =>  /\S/.test(entry)); //remove empty lines
-    arr = arr.map( val => val.trim()); //remove leading/trailing whitespace
-    arr = arr.filter( entry =>  !(/^Reopens/.test(entry))); //remove empty lines
-    arr = arr.filter( entry =>  !(/^Opens/.test(entry))); //remove empty lines
-    arr = arr.filter( entry =>  !(/^Closing/.test(entry))); //remove empty lines
-    arr = arr.filter( entry =>  !(/^\d(.\d)?$/.test(entry))); //remove rating line
-    arr = arr.filter( entry =>  !(/ · /.test(entry))); //remove reviews/pool type line
-
-    const googPools = [];
-    for (var i = 0; i < arr.length-3; i=i+2) {
-      let name = arr[i];
-      let city = arr[i+1].split(', ')[0];
-      let state = arr[i+1].split(', ')[1];
-      googPools.push( {
-        source: 'google',
-        name,
-        city,
-        state,
-      } )
-    }
-    pulsefind(googPools);
-
-    vm.text = JSON.stringify(googPools, undefined, 4);
-    console.log('googPools', googPools);
-
-    function pulseFind(list, index=0) {
-      if (index < list.length) {
-        setTimeout( () => {
-          placesSearch(list[index++]);
-          pulsePost(list, index);
-        }, 1100);
-      } else {
-        vm.searching = false;
+  //methods for parcing datasets from different sources into usable JSON
+  vm.toJson = {
+    mapMuse(text) {
+      let arr = text.split('\n'); //create array element for each line of text
+      arr = arr.filter( entry =>  /\S/.test(entry)); //remove empty lines
+      arr = arr.map( val => val.trim()); //remove leading/trailing whitespace
+      const musePools = [];
+      for (var i = 0; i < arr.length-3; i=i+3) {
+        //two capital letters followed by end of string = state
+        let state = arr[i+2].match(/[A-Z][A-Z]$/g)[0];
+        //remove the last 3 chars (state and preceding space)
+        let noState = arr[i+2].slice(0, arr[i+2].length-3);
+        //one or more words (optionally followed by a space) between ", " and end of string = city
+        let city = noState.match(/(?!, )(\w *)+(?=$)/g)[0];
+        //everything before the above ", " will be the street address
+        let street_address = noState.match(/.+(?=(, (\w *)+(?=$)))/g)[0];
+        musePools.push( {
+          source: 'mapmuse',
+          name: arr[i],
+          pool_type: arr[i+1],
+          street_address,
+          city,
+          state,
+        } )
+      }
+      vm.text = JSON.stringify(musePools, undefined, 4);
+      console.log('musePools', musePools);
+    },
+    //this section is incomplete
+    google(text) {
+      let arr = text.split('\n'); //create array element for each line of text
+      const googPools = [];
+      for (var i = 0; i < arr.length-3; i=i+2) {
+        let name = arr[i];
+        let city = arr[i+1].split(', ')[0];
+        let state = arr[i+1].split(', ')[1];
+        googPools.push( {
+          source: 'google',
+          name,
+          city,
+          state,
+        } )
       }
     }
   }
 
-
-  const mapMuse = text => {
-    let arr = text.split('\n'); //create array element for each line of text
-    arr = arr.filter( entry =>  /\S/.test(entry)); //remove empty lines
-    arr = arr.map( val => val.trim()); //remove leading/trailing whitespace
-    const musePools = [];
-    for (var i = 0; i < arr.length-3; i=i+3) {
-      //two capital letters followed by end of string = state
-      let state = arr[i+2].match(/[A-Z][A-Z]$/g)[0];
-      //remove the last 3 chars (state and preceding space)
-      let noState = arr[i+2].slice(0, arr[i+2].length-3);
-      //one or more words (optionally followed by a space) between ", " and end of string = city
-      let city = noState.match(/(?!, )(\w *)+(?=$)/g)[0];
-      //everything before the above ", " will be the street address
-      let street_address = noState.match(/.+(?=(, (\w *)+(?=$)))/g)[0];
-      musePools.push( {
-        source: 'mapmuse',
-        name: arr[i],
-        pool_type: arr[i+1],
-        street_address,
-        city,
-        state,
-      } )
-    }
-    vm.text = JSON.stringify(musePools, undefined, 4);
-    console.log('musePools', musePools);
-  }
-
-  const addFacility = facility => {
+  const addFacilityToDb = facility => {
     $http({
       method: 'POST',
       url: '/facilities/',
@@ -192,7 +164,7 @@ app.controller('AdminController', ['$http', function($http) {
       err => console.log("error adding facility: ", facility, err, vm.errorCount++) );
   };
 
-  const addPlaceId = placeId => {
+  const addPlaceIdToDb = placeId => {
     $http({
       method: 'POST',
       url: '/placeIds/',
@@ -217,9 +189,8 @@ app.controller('AdminController', ['$http', function($http) {
         facility.coords = [coords.lat, coords.lng];
         facility.zip = zipCmp.long_name;
         facility.google_place_id = locData.place_id;
-
         console.log('geocoded facility:', facility);
-        addFacility(facility);
+        addFacilityToDb(facility);
       } else {
         console.log('no location found:', url, results);
       }
