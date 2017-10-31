f420a93d3cdcb62af7a2c3ff887436d581d6a8f5
Paul Charles Dunkirk <pauldunkirk@gmail.com>
2017-10-31 10:16:04 -0500

    home page logo on hero

--- server/app.js
+++ server/app.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 const portDecision = process.env.PORT || 3000;
+
 const facilities = require('./routes/facilities');
 const photos = require('./routes/photos');
 const radar = require('./routes/radar');
-const bodyParser = require('body-parser');
 let isLocal = require('./config/config.js').local;
 
 app.get('/', (req, res) => res.sendFile(path.resolve('server/public/index.html')));
