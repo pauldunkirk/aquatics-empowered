23e4f5ed797572537cb9cc4e6e4b736b20d9ed5f
Paul Charles Dunkirk <pauldunkirk@gmail.com>
2017-12-12 13:27:39 -0600

    animation on scroll in home_controller

--- server/public/scripts/controllers/home.controller.js
+++ server/public/scripts/controllers/home.controller.js
@@ -1,3 +1,41 @@
 app.controller('HomeController', ['$location', function($location) {
       var vm = this;
+
+      var animateHTML = function () {
+        var elements,
+          windowHeight
+        var init = function () {
+          elements = document.getElementsByClassName('hidden')
+          windowHeight = window.innerHeight
+          listenForScrollAndResize()
+        }
+        var listenForScrollAndResize = function () {
+          window.addEventListener('scroll', checkPosition)
+          window.addEventListener('resize', init)
+        } //end listenForScrollAndResize
+        var checkPosition = function () {
+          for (var i = 0; i < elements.length; i++) {
+            var posFromTop = elements[i].getBoundingClientRect().top;
+            console.log('posFromTop', posFromTop);
+            if (windowHeight - posFromTop  >= 400) {
+              elements[i].className = elements[i].className.replace('hidden', 'willDefaultToIdOnThatElement')
+            } //end if
+          } //end for
+        } //end checkPosition
+        return {
+          init: init
+        } //end return of animateHTML
+      } //end animateHTML
+      animateHTML().init()
+
+
+
+
+
+
+
+
+
+
+
   }]);
